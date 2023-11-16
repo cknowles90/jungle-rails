12817fa5c6e0852ba3fa8afa139b37df19d11c32
Charles Denahy-Knowles <cjknowles90@gmail.com>
2023-11-15 16:53:35 -0800

    Added a 'total' method.

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -6,4 +6,7 @@ class LineItem < ApplicationRecord
   monetize :item_price_cents, numericality: true
   monetize :total_price_cents, numericality: true
 
+  def total 
+    quantity * (item_price_cents / 100.0)
+  end
 end
