f8fd6a3615c97d5673db636b99efe969e1567e09
Charles Denahy-Knowles <cjknowles90@gmail.com>
2023-11-17 16:08:27 -0800

    Sign Up feature added for user Authorization. SignUp route rendered; logout options and login options added.

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, confirmation: true, length: { minimum: 8 }
+
+  attr_accessor :name
+end
